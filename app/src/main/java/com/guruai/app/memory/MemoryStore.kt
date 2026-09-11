ee19b09045f58992d49b24a9b0701b1a866cd9c9
Shreya-91pandey <sp327272@gmail.com>
2026-09-11 18:11:33 +0530

    Update MemoryStore.kt

--- app/src/main/java/com/guruai/app/memory/MemoryStore.kt
+++ app/src/main/java/com/guruai/app/memory/MemoryStore.kt
@@ -1,6 +1,8 @@
 package com.guruai.app.memory
 
 import android.content.Context
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class MemoryStore(context: Context) {
     private val dao = AppDatabase.getDatabase(context).memoryDao()
@@ -11,6 +13,12 @@ class MemoryStore(context: Context) {
 
     suspend fun getAllMessages(): List<MessageEntity> {
         return dao.getAll()
+    }
+
+    suspend fun getMessagesGroupedByDate(): Map<String, List<MessageEntity>> {
+        val all = dao.getAll()
+        val formatter = SimpleDateFormat("dd MMM yyyy", Locale.getDefault())
+        return all.groupBy { formatter.format(it.timestamp) }
     }
 
     suspend fun clearAll() {
