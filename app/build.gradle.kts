8d4c39195c371990198c4d0380da6f197fb92f72
Shreya-91pandey <sp327272@gmail.com>
2026-09-14 15:56:23 +0530

    Add Google Play services Auth dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,3 +59,4 @@ dependencies {
     implementation("androidx.room:room-ktx:2.6.1")
     kapt("androidx.room:room-compiler:2.6.1")
 }
+implementation("com.google.android.gms:play-services-auth:21.2.0")
